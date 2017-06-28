8de978905982370df2e9d22833d8fd5083145ea0
mac mcdell <mac.mcdell@trader.ca>
2017-06-28 09:39:03 -0700

    update namespaces

--- Toggler/Extensions.cs
+++ Toggler/Extensions.cs
@@ -1,13 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Security.Policy;
-using System.Xml;
+
 
 namespace Toggler
 {
